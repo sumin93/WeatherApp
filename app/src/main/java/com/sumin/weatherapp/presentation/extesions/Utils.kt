72caf16382c02a04369b4de6ec63eced99358a98
sumin93 <suminfx@mail.ru>
2023-12-31 04:21:48 +0300

    Реализация FavouriteContent часть 2

--- app/src/main/java/com/sumin/weatherapp/presentation/extesions/Utils.kt
+++ app/src/main/java/com/sumin/weatherapp/presentation/extesions/Utils.kt
@@ -6,9 +6,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlin.math.roundToInt
 
 fun ComponentContext.componentScope() = CoroutineScope(
     Dispatchers.Main.immediate + SupervisorJob()
 ).apply {
     lifecycle.doOnDestroy { cancel() }
 }
+
+fun Float.tempToFormattedString(): String = "${roundToInt()}°C"
--- app/src/main/java/com/sumin/weatherapp/presentation/favourite/FavouriteContent.kt
+++ app/src/main/java/com/sumin/weatherapp/presentation/favourite/FavouriteContent.kt
@@ -1,17 +1,31 @@
 package com.sumin.weatherapp.presentation.favourite
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,9 +37,16 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.sumin.weatherapp.R
+import com.sumin.weatherapp.presentation.extesions.tempToFormattedString
 import com.sumin.weatherapp.presentation.ui.theme.CardGradients
 import com.sumin.weatherapp.presentation.ui.theme.Gradient
+import com.sumin.weatherapp.presentation.ui.theme.Orange
 
 @Composable
 fun FavouriteContent(component: FavouriteComponent) {
@@ -39,22 +60,35 @@ fun FavouriteContent(component: FavouriteComponent) {
         verticalArrangement = Arrangement.spacedBy(24.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        item(span = { GridItemSpan(2) }) {
+            SearchCard(
+                onClick = { component.onClickSearch() }
+            )
+        }
         itemsIndexed(
             items = state.cityItems,
             key = { _, item -> item.city.id }
         ) { index, item ->
             CityCard(
                 cityItem = item,
-                index = index
+                index = index,
+                onClick = { component.onCityItemClick(item.city) }
+            )
+        }
+        item {
+            AddFavouriteCityCard(
+                onClick = { component.onCLickAddFavourite() }
             )
         }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 private fun CityCard(
     cityItem: FavouriteStore.State.CityItem,
-    index: Int
+    index: Int,
+    onClick: () -> Unit
 ) {
     val gradient = getGradientByIndex(index)
     Card(
@@ -83,13 +117,109 @@ private fun CityCard(
                         radius = size.maxDimension / 2
                     )
                 }
+                .clickable { onClick() }
                 .padding(24.dp)
         ) {
+            when (val weatherState = cityItem.weatherState) {
+                FavouriteStore.State.WeatherState.Error -> {}
+
+                FavouriteStore.State.WeatherState.Initial -> {}
+
+                is FavouriteStore.State.WeatherState.Loaded -> {
+                    GlideImage(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .size(56.dp),
+                        model = weatherState.iconUrl,
+                        contentDescription = null
+                    )
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .padding(bottom = 24.dp),
+                        text = weatherState.tempC.tempToFormattedString(),
+                        color = MaterialTheme.colorScheme.background,
+                        style = MaterialTheme.typography.bodyLarge.copy(fontSize = 48.sp)
+                    )
+                }
+
+                FavouriteStore.State.WeatherState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.background
+                    )
+                }
+            }
             Text(
                 modifier = Modifier.align(Alignment.BottomStart),
                 text = cityItem.city.name,
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.background
+            )
+        }
+    }
+}
+
+@Composable
+private fun AddFavouriteCityCard(
+    onClick: () -> Unit
+) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        shape = MaterialTheme.shapes.extraLarge,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+    ) {
+        Column(
+            modifier = Modifier
+                .sizeIn(minHeight = 196.dp)
+                .fillMaxWidth()
+                .clickable { onClick() }
+                .padding(24.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .size(48.dp),
+                imageVector = Icons.Default.Edit,
+                tint = Orange,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = stringResource(R.string.button_add_favourite),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchCard(
+    onClick: () -> Unit
+) {
+    val gradient = CardGradients.gradients[3]
+    Card(
+        shape = CircleShape
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable { onClick() }
+                .fillMaxWidth()
+                .background(gradient.primaryGradient)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.background,
+                contentDescription = null,
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+            )
+            Text(
+                text = stringResource(R.string.search),
+                color = MaterialTheme.colorScheme.background,
+                modifier = Modifier.padding(16.dp)
             )
         }
     }
