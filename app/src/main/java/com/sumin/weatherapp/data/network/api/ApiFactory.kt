cb589e2eddf68a9223f19abbb20101ebf7c76443
sumin93 <suminfx@mail.ru>
2023-12-31 06:29:35 +0300

    Реализация SearchContent

--- app/src/main/java/com/sumin/weatherapp/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/sumin/weatherapp/data/network/api/ApiFactory.kt
@@ -5,10 +5,12 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.Locale
 
 object ApiFactory {
 
     private const val KEY_PARAM = "key"
+    private const val PARAM_LANG = "lang"
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
 
     private val okHttpClient = OkHttpClient.Builder()
@@ -18,6 +20,7 @@ object ApiFactory {
                 .url()
                 .newBuilder()
                 .addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY)
+                .addQueryParameter(PARAM_LANG, Locale.getDefault().language)
                 .build()
             val newRequest = originalRequest.newBuilder()
                 .url(newUrl)
--- app/src/main/java/com/sumin/weatherapp/presentation/search/SearchContent.kt
+++ app/src/main/java/com/sumin/weatherapp/presentation/search/SearchContent.kt
@@ -1,8 +1,164 @@
 package com.sumin.weatherapp.presentation.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.sumin.weatherapp.R
+import com.sumin.weatherapp.domain.entity.City
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchContent(component: SearchComponent) {
+    val state by component.model.collectAsState()
 
+    val focusRequester = remember {
+        FocusRequester()
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
+
+    SearchBar(
+        modifier = Modifier.focusRequester(focusRequester),
+        placeholder = { Text(text = stringResource(R.string.search))},
+        query = state.searchQuery,
+        onQueryChange = { component.changeSearchQuery(it) },
+        onSearch = { component.onClickSearch() },
+        active = true,
+        onActiveChange = {},
+        leadingIcon = {
+            IconButton(onClick = { component.onClickBack() }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        },
+        trailingIcon = {
+            IconButton(onClick = { component.onClickSearch() }) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            }
+        }
+    ) {
+        when (val searchState = state.searchState) {
+            SearchStore.State.SearchState.EmptyResult -> {
+                Text(
+                    text = stringResource(R.string.empty_result_text),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+
+            SearchStore.State.SearchState.Error -> {
+                Text(
+                    text = stringResource(R.string.something_went_wrong),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+
+            SearchStore.State.SearchState.Initial -> {
+
+            }
+
+            SearchStore.State.SearchState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            is SearchStore.State.SearchState.SuccessLoaded -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(
+                        items = searchState.cities,
+                        key = { it.id }
+                    ) {
+                        CityCard(
+                            city = it,
+                            onCityClick = {
+                                component.onClickCity(it)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
+
+@Composable
+private fun CityCard(
+    city: City,
+    onCityClick: (City) -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onCityClick(city) }
+                .padding(
+                    vertical = 8.dp,
+                    horizontal = 16.dp
+                )
+        ) {
+            Text(
+                text = city.name,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = city.country)
+        }
+    }
+}
+
+
+
+
+
+
+
+
